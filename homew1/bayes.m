817e4bc954de519fdf30fba7a640c193529d9177
Marco Canali <marco.canali@gmail.com>
2014-10-06 21:41:51 +0200

    Update bayes.m

--- homew1/bayes.m
+++ homew1/bayes.m
@@ -1,10 +1,10 @@
 1;
 
-# octave.m file 
+# octave.m file
 # -------------------------------------------------------------------------------------------------------------------
-# We have two opaque bags with two balls each. One has 2 black balls and the other has a black and withe ball 
-# you pick a bag at random and then pick one of the ball from the back, if the ball is balck what is the probability 
-# that the second is also black ? 
+# We have two opaque bags with two balls each. One has 2 black balls and the other has a black and withe ball
+# you pick a bag at random and then pick one of the ball from the back, if the ball is balck what is the probability
+# that the second is also black ?
 #
 # Solving this exercise using a closed-form expression
 # -------------------------------------------------------------------------------------------------------------------
@@ -23,48 +23,40 @@
 #                = (1/2 * 1) / (3/4)
 #                = 4/6
 #
-# 
-# But we interest to solving the quitz with the Machine Help 
+#
+# But we interest to solving the quitz with the Machine Help
 
 # ------------------------------------------------- set the Variable  ----------------------------------------------
-# iterations, we need more iteration to converge 
+# iterations, we need more iteration to converge
 iters = 10000;
-# balls for every bags 
-nballs = 2; 
-# the bags (with black-1 and withe-0 balls) 
+# balls for every bags
+nballs = 2;
+# the bags (with black-1 and withe-0 balls)
 bags = [[0 1] ; [1 1]];
 
 
-# loop on the experiments 
+# loop on the experiments
 for i = 1:iters
 
-    # pick at random a bag (randperm(2) -> [1 2] or [2 1] i hold only the first (1, :)) 
+    # pick at random a bag (randperm(2) -> [1 2] or [2 1] i hold only the first (1, :))
     bag = bags(randperm (length (bags)), :)(1, :);
-    
-    # shuffle the balls in the bag 
-    balls = bag(: , randpemr(nballs));
-    
-    # shuold not be considered if i pick firs the withe 
+
+    # shuffle the balls in the bag
+    balls = bag(: , randperm(nballs));
+
+    # shuold not be considered if i pick firs the withe
     if balls(1) != 1
         results(i) = 2;
         continue;
-    endif 
-    
-    # pick the ramaining ball from the bag 
+    endif
+
+    # pick the ramaining ball from the bag
     results(i) = balls(2);
-    
-endfor 
+
+endfor
 
 # remove the invalid experiment from the vector results ( results(i) = 2 , invalid )
-results = resutls(find(results != 2));
+results = results(find(results != 2));
 
-# show the results 
+# show the results
 printf("Probability of pick the bag with two black balls : %.6f\n", mean (results));
-
-
-
-
-
-
-
-
